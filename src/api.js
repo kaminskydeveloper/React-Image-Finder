ae82ddd59d47bb14cda2156af67074e30408a2bc
Damian Kamiński <kaminskydev@gmail.com>
2019-12-02 20:35:19 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://api.unsplash.com/search/photos/?client_id=5cb163c5a827aa01ffc34e7ffc47ab094674c23fdfc309b70124746c7fe3194d";
+  "";
 
 export default {
   search(searchTerm) {
